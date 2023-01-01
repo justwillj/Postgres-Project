439cb10bba7d6f79861b0adc14e0733c374f36ee
Justin Williams <williams.jman@gmail.com>
2023-01-01 16:46:05 -0500

    Created a query to see all the member stores

--- queries.sql
+++ queries.sql
@@ -1,6 +1,16 @@
---Requirment 1 
+--Requirement 1
 SELECT stores.name, address.street, address.street2, address.city, address.state, address.zip
 FROM stores
 INNER JOIN address
 ON address.id = stores.address_id
+ORDER BY name ASC;
+
+--Requirement 2
+
+
+--Requirement 3
+SELECT stores.name, customers.first_name, customers.last_name
+FROM stores
+INNER JOIN customers
+ON stores.id = store_id
 ORDER BY name ASC;
