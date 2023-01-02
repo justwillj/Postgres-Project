e5a7280ef5713de852aaf0394fe9939182ad28ed
Justin Williams <williams.jman@gmail.com>
2023-01-01 19:56:46 -0500

    Created the query to see the total items at the Administaff inc

--- queries.sql
+++ queries.sql
@@ -14,3 +14,12 @@ FROM stores
 INNER JOIN customers
 ON stores.id = store_id
 ORDER BY name ASC;
+
+--Requirement 4
+SELECT stores.name, items.name, inventory.quantity, items.price, items.notes
+FROM stores
+INNER JOIN inventory
+ON stores.id = store_id
+INNER JOIN items
+ON items.id = item_id
+WHERE stores.id = 16;
